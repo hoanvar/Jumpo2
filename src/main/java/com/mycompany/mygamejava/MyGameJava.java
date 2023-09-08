dcde8e20e3c9aa8548ae08d7f17f193745a6b8ae
FPT-SHOP <FPT-SHOP@DESKTOP-GNJ2Q4V>
2023-09-08 21:50:31 +0700

    demo

---
+++ src/main/java/com/mycompany/mygamejava/MyGameJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mygamejava;
+
+/**
+ *
+ * @author FPT-SHOP
+ */
+public class MyGameJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
